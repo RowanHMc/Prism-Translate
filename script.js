6164c58e4319654f3da7de3c7bcead867d2437fd
Rowan <rowanhadegu@gmail.com>
2026-08-03 18:11:51 +0300

    add event listener for character count and clear button

--- script.js
+++ script.js
@@ -4,7 +4,28 @@ const sourceText = document.getElementById('sourceText');
 const translatedText = document.getElementById('translatedText');
 const translateButton = document.getElementById('translateButton');
 const clearButton = document.getElementById('clearButton');
-const swapButon = document.getElementById('swapButon');
+const swapButton = document.getElementById('swapButton');
 const copyButton = document.getElementById('copyButton');
-const charachterCount = document.getElementById('charachterCount');
+const characterCount = document.getElementById('characterCount');
 const statusMessage= document.getElementById('statusMessage');
+
+// translation history - why array-
+let translationHistory = []
+const MAX_HISTORY = 10;
+
+// functions
+function updateCharacterCount(){
+    console.log(sourceText.value);
+    characterCount.textContent = sourceText.value.length;
+}
+sourceText.addEventListener("input", updateCharacterCount);
+
+function clearTranslation(){
+    sourceText.value= "";
+    translatedText.value = "";
+    characterCount.textContent = 0;
+    statusMessage = "";
+
+}
+clearButton.addEventListener("click", clearTranslation);
+
